8b2ddf2af75a5431a3b8f4b4ff55585ab860664f
DinaSof <157393086+DinaSof@users.noreply.github.com>
2024-04-28 22:50:31 +0200

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
+  console.log("poem generated");
 
   new Typewriter("#poem", {
-    strings: "Life, believe, is not a dream",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: null,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "ffd0476037477o1aaf0edt7b024c3d03";
+  let context =
+    "You are a philosophical poem expert and love to write short poems. Your mission is to generate a 4 line poem provided in HTML format, separate each line with < /br>. Example <p>a poetic line<p/>. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'by PoemD' inside a <strong> element at the end of the poem.";
+  let prompt = `User instructions are: Generate a poem about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
