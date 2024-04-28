3a97ff3c0df884c62104727b1dea7e5fe8332884
DinaSof <157393086+DinaSof@users.noreply.github.com>
2024-04-28 22:53:54 +0200

    Javascript
    prompts clarification

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "ffd0476037477o1aaf0edt7b024c3d03";
   let context =
-    "You are a philosophical poem expert and love to write short poems. Your mission is to generate a 4 line poem provided in HTML format, separate each line with < /br>. Example <p>a poetic line<p/>. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'by PoemD' inside a <strong> element at the end of the poem.";
+    "You are a philosophical poem expert and love to write short poems. Your mission is to generate a 4 line poem provided in HTML format, separate each line with < /br>. Example <p>a poetic line<p/>. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'by PoemD' inside a <strong> element, at the end of the poem.";
   let prompt = `User instructions are: Generate a poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
